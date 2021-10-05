6c4a314d49dc03a4a5677ee4ab510ca89488577d
dong <810776886@qq.com>
2021-10-05 12:08:37 +0800

    zmalloc_set_oom_handler  redisOutOfMemoryHandler

--- src/latte_redis_server.c
+++ src/latte_redis_server.c
@@ -1,5 +1,9 @@
 #include "latte_redis_server.h"
 #include "util/util.h"
+#include <locale.h>
+#include <time.h>
+#include <sys/time.h>
+#include "log/log.h"
 // #include "client.h"
 struct redisServer server;
 void getCommand(client *c) {
@@ -23,9 +27,24 @@ struct redisCommand redisCommandTable[] = {
        KSPEC_FK_RANGE,.fk.range={0,1,0}}}}
 };
 
+void redisOutOfMemoryHandler(size_t allocation_size) {
+    serverLog(LL_WARNING,"Out Of Memory allocating %zu bytes!",
+        allocation_size);
+    panic("Redis aborting for OUT OF MEMORY. Allocating %zu bytes!",
+        allocation_size);
+}
 
 
-
-int main() {
+int main(int argc, char **argv) {
+    struct timeval tv;
+    int j;
+    char config_from_stdin = 0;
+      /* We need to initialize our libraries, and the server configuration. */
+#ifdef INIT_SETPROCTITLE_REPLACEMENT
+    spt_init(argc, argv);
+#endif
+    setlocale(LC_COLLATE,"");
+    tzset();
+    zmalloc_set_oom_handler(redisOutOfMemoryHandler);
     return 1;
 }
