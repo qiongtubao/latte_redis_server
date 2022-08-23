5756e22645945df5b41e962aae589725bee8f826
dong <810776886@qq.com>
2022-08-23 14:50:08 +0800

    init server use latte_c lib

--- src/robj/types/module.c
+++
@@ -1,9 +0,0 @@
-#include "../robj.h"
-#include "module.h"
-
-
-void freeModuleObject(robj *o) {
-    moduleValue *mv = o->ptr;
-    mv->type->free(mv->value);
-    zfree(mv);
-}
--- src/robj/types/string.c
+++
@@ -1,8 +0,0 @@
-#include "../robj.h"
-
-
-void freeStringObject(robj *o) {
-    if (o->encoding == OBJ_ENCODING_RAW) {
-        sdsfree(o->ptr);
-    }
-}
